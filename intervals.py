ce4f4b691bca9a66ed392d1c7b28efe4a15cbd3e
Oscar Byrne <oscar.byrne@gmail.com>
2015-12-17 10:32:42 +0000

    First commit

---
+++ intervals.py
@@ -0,0 +1,34 @@
+__all__ = ['P1', 'd2', 'm2', 'A1', 'M2', 'd3', 'm3', 'A2', 'M3', 'd4', 'P4', 'A3', 'd5', 'A4', 'P5', 'd6', 'm6', 'A5', 'M6', 'd7', 'm7', 'A6', 'M7', 'd8', 'P8', 'A7']
+
+
+class Interval(int):
+
+    pass
+
+
+P1 = Interval(0)
+d2 = Interval(0)
+m2 = Interval(1)
+A1 = Interval(1)
+M2 = Interval(2)
+d3 = Interval(2)
+m3 = Interval(3)
+A2 = Interval(3)
+M3 = Interval(4)
+d4 = Interval(4)
+P4 = Interval(5)
+A3 = Interval(5)
+d5 = Interval(6)
+A4 = Interval(6)
+P5 = Interval(7)
+d6 = Interval(7)
+m6 = Interval(8)
+A5 = Interval(8)
+M6 = Interval(9)
+d7 = Interval(9)
+m7 = Interval(10)
+A6 = Interval(10)
+M7 = Interval(11)
+d8 = Interval(11)
+P8 = Interval(12)
+A7 = Interval(12)
